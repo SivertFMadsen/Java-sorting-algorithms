56eb5856caa6e37a056bf292e4eecb0e3722eb5e
TheTubanator <sivertfm@gmail.com>
2022-10-05 12:02:12 +0200

    Insertion sort implemented

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,23 +1,33 @@
 /**
- * Sorts an array in O(n^2) time by comparing to neighbouring
- * elements, switching them if they are in the wrong order, and
- * continues likewise through the array until everything is sorted.
+ * Sorts an array in (at worst) O(n^2) time by comparing to 
+ * neighbouring elements, switching them if they are in the 
+ * wrong order, and continues likewise through the array 
+ * until everything is sorted.
  */
 public class InsertionSort {
 
     public static int[] sort(int[] array) {
 
+        int numSwitches = 0;
+
         for (int i = 1; i < array.length; i++) {
             int j = i;
             while (j > 0 && array[j-1] > array[j]) {
-                // Switches two neighbouring elements
+                /**
+                 * Switches two neighbouring elements
+                 * if they are in the wrong order, and 
+                 * continues likewise backwards through 
+                 * the array from position 'i'
+                 */ 
                 int temp = array[j-1];
 
                 array[j-1] = array[j];
                 array[j] = temp;
+                numSwitches += 1;
                 j -= 1;
             }
         }
+        // System.out.println("Number of switches: " + numSwitches);
         return array;
     }
 }
--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     
     public static void main(String[] args) {
         
-        int[] unSorted = {9, 1, 5, 6, 4, 3, 7, 2, 10};
+        int[] unSorted = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
         int[] sorted = InsertionSort.sort(unSorted);
 
         for (int num : sorted) {
