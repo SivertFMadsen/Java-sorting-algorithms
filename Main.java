2c62d3a850062880f2da6a2dd1da0af659d2d47b
TheTubanator <sivertfm@gmail.com>
2022-10-05 13:37:41 +0200

    Quicksort implemented

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         
         int[] unSorted = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        int[] sorted = InsertionSort.sort(unSorted);
+        int[] sorted = QuickSort.sort(unSorted);
 
         for (int num : sorted) {
             System.out.println(num);
--- QuickSort.java
+++ QuickSort.java
@@ -2,28 +2,56 @@
  */
 public class QuickSort {
 
+    static int changes;
+
     public static int[] sort(int[] array) {
 
-        array = sort_r(array, 0, array.length - 1);
+        sort_r(array, 0, array.length - 1);
 
         return array;
     }
 
     private static int[] sort_r(int[] array, int low, int high) {
 
+        if (low >= high) {
+            return array;
+        }
+
+        int p = partition(array, low, high);
+        sort_r(array, low, p - 1);
+        sort_r(array, p + 1, high);
 
         return array;
     }
 
-    private static int[] partition(int[] part, int low, int high) {
+    private static int partition(int[] part, int low, int high) {
         
-        int pivot = (part.length - 1)/2;
-        int temp = part[pivot];
-        part[pivot] = part[high];
-        part[high] = part[temp];
+        int temp;
+        int pivotIndex = low + ((high - low)/2);
+        int pivot = part[pivotIndex];
+        part[pivotIndex] = part[high];
+        part[high] = pivot;
         
+        int left = low;
+        int right = high - 1;
 
+        while (left <= right) {
+            while (left <= right && part[left] <= pivot) {
+                left ++;
+            }
+            while (right >= left && part[right] >= pivot) {
+                right --;
+            }
+            if (left < right) {
+                temp = part[left];
+                part[left] = part[right];
+                part[right] = temp;
+            }
+        }
+        temp = part[left];
+        part[left] = part[high];
+        part[high] = temp;
 
-        return part;
+        return left;
     }
 }
